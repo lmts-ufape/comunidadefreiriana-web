fe21a4e04c275596d7dd148a32ea547422fee67c
Edgarvital <edgar.tecnico@hotmail.com>
2022-10-02 16:48:02 -0300

    Trocando vários campos de instituição para nullable e fazendo algumas modificações na exibição dessas instituições

--- resources/views/instituicao/edit.blade.php
+++ resources/views/instituicao/edit.blade.php
@@ -47,7 +47,7 @@
                             </div>
                             <div class="form-row">
                                 <div class="col-md-6 form-group">
-                                    <label for="nome">Nome</label>
+                                    <label for="nome">Nome<strong>*</strong></label>
                                     <input type="text" id="nome" name="nome" class="form-control @error('nome') is-invalid @enderror" autofocus required value="{{old('nome', $instituicao->nome)}}">
 
                                     @error('nome')
@@ -57,14 +57,14 @@
                                     @enderror
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for="categoria">Categoria</label>
+                                    <label for="categoria">Categoria<strong>*</strong></label>
                                     <select type="text" id="categoria" name="categoria" class="form-control @error('categoria') is-invalid @enderror">
                                         <option value="">Selecione uma categoria</option>
-                                        <option <?php if ($instituicao->categoria=='Instituto Paulo Freire') echo 'selected' ; ?> value="Instituto Paulo Freire">Instituto Paulo Freire</option>
-                                        <option <?php if ($instituicao->categoria=='Cátedra Paulo Freire') echo 'selected' ; ?> value="Cátedra Paulo Freire">Cátedra Paulo Freire</option>
-                                        <option <?php if ($instituicao->categoria=='Centro Paulo Freire') echo 'selected' ; ?> value="Centro Paulo Freire">Centro Paulo Freire</option>
-                                        <option <?php if ($instituicao->categoria=='Grupo/Coletivo Paulo Freire') echo 'selected' ; ?> value="Grupo/Coletivo Paulo Freire">Grupo/Coletivo Paulo Freire</option>
-                                        <option <?php if ($instituicao->categoria=='Homenagem') echo 'selected' ; ?> value="Homenagem">Homenagem</option>
+                                        <option @if($instituicao->categoria == 'Instituto Paulo Freire') selected @endif value="Instituto Paulo Freire">Instituto Paulo Freire</option>
+                                        <option @if($instituicao->categoria == 'Cátedra Paulo Freire') selected @endif value="Cátedra Paulo Freire">Cátedra Paulo Freire</option>
+                                        <option @if($instituicao->categoria == 'Centro Paulo Freire') selected @endif value="Centro Paulo Freire">Centro Paulo Freire</option>
+                                        <option @if($instituicao->categoria == 'Grupo/Coletivo Paulo Freire') selected @endif value="Grupo/Coletivo Paulo Freire">Grupo/Coletivo Paulo Freire</option>
+                                        <option @if($instituicao->categoria == 'Homenagem') selected @endif value="Homenagem">Homenagem</option>
                                     </select>
                                     @error('categoria')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -75,14 +75,14 @@
                             </div>
                             <div class="form-row">
                                 <div class="col-md-6 form-group">
-                                    <label for="pais">Pais</label>
+                                    <label for="pais">Pais<strong>*</strong></label>
                                     <select type="text" id="pais" name="pais" class="form-control @error('pais') is-invalid @enderror" required>
                                         <option value="">Selecione um país</option>
                                         @php
                                             $paises = \App\Countries::getLista();
                                         @endphp
                                         @foreach($paises as $key => $nome)
-                                            <option <?php if ($instituicao->pais==$nome) echo 'selected' ; ?> value="{{$nome}}">{{$nome}}</option>
+                                            <option @if ($instituicao->pais==$nome) selected @endif value="{{$nome}}">{{$nome}}</option>
                                         @endforeach
                                     </select>
                                     @error('pais')
@@ -93,7 +93,7 @@
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for="cep">Cep</label>
-                                    <input type="text" id="cep" name="cep" class="form-control @error('cep') is-invalid @enderror" required value="{{old('cep', $instituicao->cep)}}">
+                                    <input type="text" id="cep" name="cep" class="form-control @error('cep') is-invalid @enderror" value="{{old('cep', $instituicao->cep)}}">
 
                                     @error('cep')
                                     <div id="validationServer03Feedback" class="invalid-feedback">
@@ -106,7 +106,7 @@
 
                                 <div class="col-md-6 form-group">
                                     <label for="estado">Estado</label>
-                                    <input type="text" id="estado" name="estado" class="form-control @error('estado') is-invalid @enderror" required value="{{old('estado', $instituicao->estado)}}">
+                                    <input type="text" id="estado" name="estado" class="form-control @error('estado') is-invalid @enderror" value="{{old('estado', $instituicao->estado)}}">
 
                                     @error('estado')
                                     <div id="validationServer03Feedback" class="invalid-feedback">
@@ -116,7 +116,7 @@
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for="cidade">Cidade</label>
-                                    <input type="text" id="cidade" name="cidade" class="form-control @error('cidade') is-invalid @enderror" required value="{{old('cidade', $instituicao->cidade)}}">
+                                    <input type="text" id="cidade" name="cidade" class="form-control @error('cidade') is-invalid @enderror" value="{{old('cidade', $instituicao->cidade)}}">
 
                                     @error('cidade')
                                     <div id="validationServer03Feedback" class="invalid-feedback">
@@ -129,7 +129,7 @@
 
                                 <div class="col-md-6 form-group">
                                     <label for="endereco">Endereço</label>
-                                    <input type="text" id="endereco" name="endereço" class="form-control @error('endereço') is-invalid @enderror" required value="{{old('endereço', $instituicao->endereco)}}">
+                                    <input type="text" id="endereco" name="endereço" class="form-control @error('endereço') is-invalid @enderror" value="{{old('endereço', $instituicao->endereco)}}">
 
                                     @error('endereço')
                                     <div id="validationServer03Feedback" class="invalid-feedback">
@@ -139,7 +139,7 @@
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for="telefone">Telefone</label>
-                                    <input type="text" id="telefone" name="telefone" class="form-control @error('telefone') is-invalid @enderror" required value="{{old('telefone', $instituicao->telefone)}}">
+                                    <input type="text" id="telefone" name="telefone" class="form-control @error('telefone') is-invalid @enderror" value="{{old('telefone', $instituicao->telefone)}}">
 
                                     @error('telefone')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -151,7 +151,7 @@
                             <div class="form-row">
                                 <div class="col-md-6 form-group">
                                     <label for="email">Email</label>
-                                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" required value="{{old('email', $instituicao->email)}}">
+                                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value="{{old('email', $instituicao->email)}}">
 
                                     @error('email')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -161,7 +161,7 @@
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for="site">Site</label>
-                                    <input type="text" id="site" name="site" class="form-control @error('site') is-invalid @enderror" required value="{{old('site', $instituicao->site)}}">
+                                    <input type="text" id="site" name="site" class="form-control @error('site') is-invalid @enderror" value="{{old('site', $instituicao->site)}}">
 
                                     @error('site')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -173,7 +173,7 @@
                             <div class="form-row">
                                 <div class="col-md-6 form-group">
                                     <label for="datafundacao">Data da fundação</label>
-                                    <input type="date" id="datafundacao" name="data_de_fundação" class="form-control @error('data_de_fundação') is-invalid @enderror" required value="{{old('data_de_fundação', $instituicao->datafundacao)}}">
+                                    <input type="date" id="datafundacao" name="data_de_fundação" class="form-control @error('data_de_fundação') is-invalid @enderror" value="{{old('data_de_fundação', $instituicao->datafundacao)}}">
 
                                     @error('data_de_fundação')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -183,7 +183,7 @@
                                 </div>
                                 <div class="col-md-6 form-group">
                                     <label for="coordenador">Coordenador</label>
-                                    <input type="text" id="coordenador" name="coordenador" class="form-control @error('coordenador') is-invalid @enderror" required value="{{old('coordenador', $instituicao->coordenador)}}">
+                                    <input type="text" id="coordenador" name="coordenador" class="form-control @error('coordenador') is-invalid @enderror" value="{{old('coordenador', $instituicao->coordenador)}}">
 
                                     @error('coordenador')
                                         <div id="validationServer03Feedback" class="invalid-feedback">
@@ -194,7 +194,7 @@
                             </div>
                             <div class="form-row">
                                 <div class="col-md-6 form-group">
-                                    <label for="latitude">Latitude</label>
+                                    <label for="latitude">Latitude<strong>*</strong></label>
                                     <input type="number" min="-90" max="90" step="0.000001" name="latitude" class="form-control @error('latitude') is-invalid @enderror" required @if(old('latitude')) value="{{old('latitude')}}" @else value="{{$instituicao->latitude}}" @endif>
 
                                     @error('latitude')
@@ -204,7 +204,7 @@
                                     @enderror
                                 </div>
                                 <div class="col-md-6 form-group">
-                                    <label for="longitude">Longitude</label>
+                                    <label for="longitude">Longitude<strong>*</strong></label>
                                     <input type="number" min="-180" max="180" step="0.000001" id="longitude" name="longitude" class="form-control @error('longitude') is-invalid @enderror" required @if(old('longitude')) value="{{old('longitude')}}" @else value="{{$instituicao->longitude}}" @endif>
 
                                     @error('longitude')
